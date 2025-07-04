3383b729378ce60803b8df6d46e7c12a3b470897
naufal <naufalfachry76@gmail.com>
2025-07-04 14:18:42 +0700

    wa

--- src/components/whatsapp.tsx
+++ src/components/whatsapp.tsx
@@ -1,17 +1,19 @@
 'use client'
 
-import { Link } from 'lucide-react'
+import Link from 'next/link'
 import { FaWhatsapp } from 'react-icons/fa'
 
-export default function WhatsAppButton() {
+export default function FloatingWhatsAppButton() {
   return (
-    <Link
-      href="https://wa.me/6287793942392"
-      target="_blank"
-      className="fixed bottom-6 right-6 z-50 bg-[#f22a98]  hover:bg-black hover:shadow-[0_0_10px_#f22a98] text-white p-4 rounded-full shadow-lg transition duration-300"
-      aria-label="Chat via WhatsApp"
-    >
-      <FaWhatsapp size={30} />
-    </Link>
+    <a
+  href="https://wa.me/6287793942392"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="fixed bottom-6 right-6 z-50 bg-[#f22a98] hover:bg-black hover:shadow-[0_0_10px_#f22a98] text-white p-4 rounded-full shadow-lg transition duration-300"
+  aria-label="Chat via WhatsApp"
+>
+  <FaWhatsapp className="text-2xl" />
+</a>
   )
 }
+
